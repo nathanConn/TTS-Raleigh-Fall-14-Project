fe819d67caaffb679dc3d3ee60205dd637ff91f9
Aaron <williamstrouse@Aaron.local>
2014-11-25 11:36:05 -0500

    tuesday version

--- db/migrate/20141124155101_delete_users_table.rb
+++
@@ -1,4 +0,0 @@
-class DeleteUsersTable < ActiveRecord::Migration
-  def change
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141124173917) do
+ActiveRecord::Schema.define(version: 20141124164122) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "comments", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "user_id"
     t.string   "post_id"
-  end
-
-  create_table "comments_posts", id: false, force: true do |t|
-    t.integer "post_id",    null: false
-    t.integer "comment_id", null: false
   end
 
   create_table "posts", force: true do |t|
@@ -32,6 +30,15 @@ ActiveRecord::Schema.define(version: 20141124173917) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "user_id"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.boolean  "administrator"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
