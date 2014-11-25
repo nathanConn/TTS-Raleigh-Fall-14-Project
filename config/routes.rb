cd4c20d7ba71db9f464769560f6052e61a8aacaa
Brandon <btbaum11@gmail.com>
2014-11-25 11:51:12 -0500

    Updated Post Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
+  
   resources :posts
-
+  
   get 'users/:id' => 'users#show'
+
+  get 'posts/view_post'
+
+  get 'posts/create_post'
+
+
+  root to: 'posts#index'
+  
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
