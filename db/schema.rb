96d59fb2dc807cb020a6be2f6f3d02016b81abd6
Aaron <williamstrouse@Aaron.local>
2014-11-26 09:46:15 -0500

    added body to comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141125182054) do
+ActiveRecord::Schema.define(version: 20141126143149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20141125182054) do
     t.datetime "updated_at"
     t.string   "user_id"
     t.string   "post_id"
+    t.string   "body"
   end
 
   create_table "posts", force: true do |t|
