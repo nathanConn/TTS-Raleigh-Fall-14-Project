5d5d7e8e541273734b231031d62f0c945dd988b5
Rick <rickpittman8@gmail.com>
2014-12-02 12:16:06 -0500

    routes, csrf_meta_tag

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
 
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
-  get 'logout' => 'sessions#destroy'
+  delete 'logout' => 'sessions#destroy'
   resources :posts
   resources :users
 
