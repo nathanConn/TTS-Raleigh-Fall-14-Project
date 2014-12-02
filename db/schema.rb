ef8a724c68dace84edae3585e60c6928090db607
Brandon <btbaum11@gmail.com>
2014-12-02 11:21:45 -0500

    Removed Author from Post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141126143149) do
+ActiveRecord::Schema.define(version: 20141202160034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,12 +24,8 @@ ActiveRecord::Schema.define(version: 20141126143149) do
     t.string   "body"
   end
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "posts", force: true do |t|
     t.string   "title"
-    t.string   "author"
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -45,6 +41,7 @@ ActiveRecord::Schema.define(version: 20141126143149) do
     t.datetime "updated_at"
     t.string   "username"
     t.string   "password"
+    t.string   "password_digest"
   end
 
 end
