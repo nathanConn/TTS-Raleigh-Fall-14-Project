38f52f2dd1f4aa2845cc64dfe09fe0b48b79a069
NATHAN <nathanconn1@gmail.com>
2014-12-02 10:29:43 -0500

    dynamic homepage links to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,7 +69,6 @@ class PostsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
-      # @post = Post.find(params[:id])
       @post = Post.find(params[:id]) 
     end
 
