8fd94accf8f7ecab06f7a4f7bd259e999692ce60
Rick <rickpittman8@gmail.com>
2014-12-04 00:53:36 -0500

    account activation and mailer setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-	attr_accessor :remember_token, :activation_token
+	attr_accessor :remember_token, :activation_token, :reset_token
   	before_save   :downcase_email
     before_create :create_activation_digest
 
@@ -42,6 +42,33 @@ class User < ActiveRecord::Base
   	return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
     end
+
+    # Activates an account.
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
 
     private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root 'home#show'
   
   
@@ -24,6 +28,7 @@ Rails.application.routes.draw do
   resources :posts
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
  
 
