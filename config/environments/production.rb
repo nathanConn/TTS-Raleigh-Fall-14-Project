a1846fda02abc1f7d09dff179e51ab3b3ee977a4
Rick <rickpittman8@gmail.com>
2014-12-15 23:01:05 -0500

    changed hostname

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'damp-refuge-2926.herokuapp.com'
+  host = 'techtalentsouth-codez.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
