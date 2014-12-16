03924a4d41f094a45a33654c3f87a0adf206eeb9
Rick <rickpittman8@gmail.com>
2014-12-16 01:58:50 -0500

    send

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,8 +8,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['APP32557888@HEROKU.COM'],
-    :password       => ENV['SHIP97RVR'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
