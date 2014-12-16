45d507e17477a36dfab95878be4389148edfc68f
Rick <rickpittman8@gmail.com>
2014-12-16 01:20:10 -0500

    send config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,14 +2,14 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'smtp.sendgrid.net'
+  host = 'techtalentsouth-codez.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => ENV['app32557888@heroku.com'],
+    :password       => ENV['Ship97rvr'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
